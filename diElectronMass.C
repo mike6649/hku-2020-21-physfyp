64937296649cb8ea9f62a0f0db79aaf7533bc926
Michael Tang <michaeltang6649@gmail.com>
2020-11-05 19:52:11 +0800

    Update ROOT macro

--- diElectronMass.C
+++ diElectronMass.C
@@ -4,9 +4,10 @@
 #include "TH1F.h"
 #include "TClonesArray.h"
 #include <iostream>
+
 using namespace std;
 
-void test (char* fileName) {
+void eMass (char* fileName) {
   // Create chain of root trees
   TChain chain("Delphes");
   chain.Add(fileName);
@@ -19,8 +20,7 @@ void test (char* fileName) {
   TClonesArray *branchElectron = treeReader->UseBranch("Electron");
 
   // Book histograms
-  TH1 *histElectronPT = new TH1F("electron pt", "electron P_{T}", 50, 0.0, 100.0);
-
+  TH1 *histElectronPT = new TH1F("M", "M_{inv}(e_{1},e_{2})", 100, 40, 140);
   // Loop over all events
   for(Int_t entry = 0; entry < numberOfEntries; ++entry)
   {
@@ -28,17 +28,19 @@ void test (char* fileName) {
     // Load selected branches with data from specified event
     treeReader->ReadEntry(entry);
   
-    // If event contains at least 1 electron
-    if (branchElectron->GetEntries() > 0)
+    if (branchElectron->GetEntries() > 1)
     {
-      // Take first electron
-      Electron *electron = (Electron*) branchElectron->At(0);
+
+      Electron *e1 = (Electron*) branchElectron->At(0);
+      Electron *e2 = (Electron*) branchElectron->At(1);
       
-      // Plot electron transverse momentum
-      histElectronPT->Fill(electron->PT);
-      
-      // Print electron transverse momentum
-      cout << electron->PT << endl;
+      Double_t invMass = (e1->P4()+e2->P4()).M();
+
+      // Plot electron mass
+      histElectronPT->Fill(invMass);
+
+      // Print electron mass
+      cout << invMass << endl;
     }
 
   }
